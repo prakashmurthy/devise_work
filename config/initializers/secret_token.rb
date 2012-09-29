3ff387a94db5dc38464c2a857d7120075ad8abd7
Prakash Murthy <prakblr@yahoo.com>
2012-09-29 15:49:17 -0600

    Initial commit. After 'rails g devise:install'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseWork::Application.config.secret_token = '326b6eae5d847430bc72a806457c29bdd4082925fef0fbb1584a8fc118023643dfee8d6d14a6498bc21311e8f8a41c72eace2c3b3bb7d79d96d9bca51c166add'
