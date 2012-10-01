449002837f84971e77eb3f3db571451fa063d03b
Prakash Murthy <prakblr@yahoo.com>
2012-09-30 22:36:44 -0600

    Customized devise views to add 'username', and made in/out logic work for newly signed up users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,13 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :name, :custom_status, :logged_in
   # attr_accessible :title, :body
+  validates :name, :presence => true
 
   def online?
     updated_at > 10.minutes.ago
   end
 
   def status
-    self.logged_in && self.online? ? "IN" : "OUT"     
+    (self.logged_in || self.logged_in.nil? ) && self.online? ? "IN" : "OUT"     
   end
 end
