5cbb5df9ebaad47d6a099c0d1e6bef4a2dff53cf
Prakash Murthy <prakblr@yahoo.com>
2012-10-01 12:04:22 -0600

    Fixed the bug with 'Modify custom status'

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,26 +1,27 @@
 $(document).ready(function(){
     var edit_mode=false;
-    $("#custom-status-button").click(function(){
-      var that=$(this)
+    $(".custom-status-button").click(function(evt){
+      var btn=$(evt.target)
+      var cell=btn.closest(".user-container").find(".custom-status");
       if (edit_mode){
-        var custom_status_value=$("#custom-status input").val();
+        var custom_status_value=$(".custom-status input").val();
         $.ajax({
           "type":"POST",
           "url":"/dashboard/update_custom_status",
           "data":{"status":custom_status_value},
           "success":function(){
-            that.html("Modify Custom Status");
+            btn.html("Modify Custom Status");
             edit_mode=false
-            $("#custom-status").html( custom_status_value );
+            cell.html( custom_status_value );
           }
         })
       }
       else{
-        var custom_status_value=$("#custom-status").html();
+        var custom_status_value=cell.text();
         var text_field=$("<input type='text'>");
         text_field.attr( "value",custom_status_value ); 
-        $("#custom-status").html( text_field );
-        that.html("Set Custom Status");
+        cell.html( text_field );
+        btn.html("Set Custom Status");
         edit_mode=true
       }
     })
