b2524069ccfe24177b103a6ea9de29f82640248f
Prakash Murthy <prakblr@yahoo.com>
2012-10-21 16:28:19 -0600

    Added ability to log failed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,22 @@
 class SessionsController < Devise::SessionsController
+  after_filter :log_failed_login, :only => :new
   def create
     super
+    ::Rails.logger.info "\n***\nSuccessful login with email_id : #{request.filtered_parameters["user"]}\n***\n"
     current_user.update_attributes!( :logged_in => true )
   end
 
   def destroy
     current_user.update_attributes!( :logged_in => false )
     super
+  end
+
+  private
+  def log_failed_login
+    ::Rails.logger.info "\n***\nFailed login with email_id : #{request.filtered_parameters["user"]}\n***\n" if failed_login?
+  end
+
+  def failed_login?
+    (options = env["warden.options"]) && options[:action] == "unauthenticated"
   end
 end
